aa46e05f45dc7ca50ffd9d212cedbf4b11d8f7e1
Bruno Gurgel <bmg1612@gmail.com>
2020-11-24 21:05:31 -0300

    feat: Set the project environment ->
    - Webpack,
    - Loaders,
    - Plugins,
    - Jest,
    - Workbox

---
+++ webpack.prod.js
@@ -0,0 +1,56 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/client/index.js',
+    devtool: 'source-map',
+    optimization: {
+        minimizer: [new OptimizeCSSAssetsPlugin({})],
+
+    },
+    // output: {
+    //     libraryTarget: 'var',
+    //     library: 'Client',
+    // },
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }), 
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+            sourcemap: true,
+            //Was not founding the files because there whas an 'auto' before them
+            // modifyURLPrefix: {
+            //     'auto./': '',
+            //     'auto':''
+            // },
+            // inlineWorkboxRuntime: true   
+        }),
+    ]
+}
+
