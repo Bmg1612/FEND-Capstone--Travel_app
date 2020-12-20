f9140a905b54353578d8daa5c68cc9c93c490b6c
Bruno Gurgel <bmg1612@gmail.com>
2020-12-20 11:26:33 -0300

    docs: Added toDoList function to the 'app' file

--- src/client/index.js
+++ src/client/index.js
@@ -7,6 +7,5 @@ import {
   preFillToDoData,
 } from './js/localStorage';
 import { getTravelResults } from './js/app';
-import { toDoFunction } from './js/toDoList';
 
 import './styles/main.scss';
--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,6 +2,7 @@ import {
   storageAvailable,
   saveTripData,
   preFillTripData,
+  saveToDoData,
   preFillToDoData,
 } from './localStorage';
 
@@ -56,6 +57,7 @@ const getTravelResults = document.addEventListener(
       init() {
         view.init();
         this.mainFunction();
+        this.toDoList();
       },
       /**
        * Changes the model.input properties to the values that the user entendered.
@@ -175,6 +177,77 @@ const getTravelResults = document.addEventListener(
               })
             )
             .then(() => view.render());
+        });
+      },
+      toDoList() {
+        view.form.addEventListener('submit', () => {
+          // Adding timeout because the 'span' event depends on the creation
+          // of the element, which occurs when the form is submitted.
+          setTimeout(() => {
+            console.log('Ready.');
+
+            const list = document.getElementById('myUL');
+            /**
+             * Add a "checked" symbol when clicking on a list item and remove the item if the 'X' is clicked.
+             * @event
+             * @async
+             * @returns {void} Nothing.
+             */
+            list.addEventListener('click', (event) => {
+              if (event.target.tagName === 'LI') {
+                event.target.classList.toggle('checked');
+                // Erasing the item if the respective 'x' is clicked
+              } else if (
+                event.target.tagName === 'SPAN' &&
+                event.target.parentElement.tagName === 'LI'
+              ) {
+                const itemToBeRemoved = event.target.parentElement;
+                itemToBeRemoved.remove();
+                // Erasing the item from the local storage too
+                for (const value of Object.values(localStorage)) {
+                  const deletedItemInnerText = itemToBeRemoved.innerText;
+                  if (deletedItemInnerText.includes(value)) {
+                    localStorage.removeItem(
+                      Object.keys(localStorage).find(
+                        (key) => localStorage[key] === value
+                      )
+                    );
+                  }
+                }
+              }
+            });
+
+            const addButton = document.querySelector('.addBtn');
+            /**
+             * Create a new list item when clicking on the "Add" button.
+             * @event
+             * @async
+             * @returns {void} Nothing.
+             */
+            addButton.addEventListener('click', () => {
+              const li = document.createElement('li');
+              const allItems = document.querySelectorAll('li');
+              const inputValue = document.getElementById('myInput').value;
+              li.textContent = inputValue;
+              if (inputValue === '') {
+                alert('You must write something!');
+              } else if (allItems.length <= 5) {
+                document.getElementById('myUL').appendChild(li);
+                if (storageAvailable('localStorage')) {
+                  saveToDoData();
+                }
+              } else {
+                alert('There is no more space for items. Sorry!');
+              }
+              document.getElementById('myInput').value = '';
+
+              const span = document.createElement('SPAN');
+              const txt = document.createTextNode('\u00D7');
+              span.className = 'close';
+              span.appendChild(txt);
+              li.appendChild(span);
+            });
+          }, 5000);
         });
       },
     };
@@ -362,6 +435,8 @@ const getTravelResults = document.addEventListener(
       init() {
         this.form = document.querySelector('.form');
         this.resultsDiv = document.getElementById('results');
+        /* this.list = document.getElementById('myUL');
+        this.addButton = document.querySelector('.addBtn'); */
       },
 
       /**
